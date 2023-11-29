0d3b5da3c50f4cd4df110603989b2bab3ead1b42
em-wb <131539794+em-wb@users.noreply.github.com>
2023-11-29 17:55:14 +0100

    Add data from weather api

--- src/App.js
+++ src/App.js
@@ -1,69 +1,118 @@
-import React from "react";
-// import axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 export default function App() {
-  let weatherData = {
-    city: "London",
-    temperature: 22,
-    date: "Monday 12:00",
-    description: "Cloudy",
-    imgUrl: "https://openweathermap.org/img/wn/03d@2x.png",
-    humidity: 10,
-    wind: 15,
-  };
+  //   let weatherData = {
+  //     city: "London",
+  //     temperature: 22,
+  //     date: "Monday 12:00",
+  //     description: "Cloudy",
+  //     imgUrl: "https://openweathermap.org/img/wn/03d@2x.png",
+  //     humidity: 10,
+  //     wind: 15,
+  //   };
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState("Bordeaux");
+
+  function getWeather(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      date: "Monday 12:00",
+      temp: Math.round(response.data.main.temp),
+      desc: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed * 3.6),
+      imgId: response.data.weather[0].icon,
+    });
+  }
+
+  let getInput = (e) => setCity(e.target.value);
+
+  function handleSumbit(e) {
+    e.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+    fetch(url).then((response) => {
+      if (response.ok) {
+        axios.get(url).then(getWeather);
+      }
+    });
+  }
 
   return (
     <div className="App">
       <div className="container">
         <div className="app-ctr dark-light">
-          <form id="search-cities" class="row search-ctr" action="">
-            <div class="col d-flex">
-              <div class="col-10">
-                <input id="new-city" type="text" placeholder="Enter a city" />
+          <form
+            onSubmit={handleSumbit}
+            id="search-cities"
+            className="row search-ctr"
+            action=""
+          >
+            <div className="col d-flex">
+              <div className="col-10">
+                <input
+                  onChange={getInput}
+                  id="new-city"
+                  type="text"
+                  placeholder="Enter a city"
+                />
               </div>
-              <div class="btn-ctr d-flex col-md-2">
+              <div className="btn-ctr d-flex col-md-2">
                 <button type="submit">
-                  <i class="fa-solid fa-magnifying-glass" alt="Search"></i>
+                  <i className="fa-solid fa-magnifying-glass" alt="Search"></i>
                 </button>
                 <button
                   type="button"
                   id="current-location-btn"
                   alt="Current location"
                 >
-                  <i class="fa-solid fa-location-dot"></i>
+                  <i className="fa-solid fa-location-dot"></i>
                 </button>
               </div>
             </div>
           </form>
-          <div class="row current-ctr mt-2">
-            <h1 class="col-sm-4 current city-cell" id="current-city">
+          <div className="row current-ctr mt-2">
+            <h1 className="col-sm-4 current city-cell" id="current-city">
               {weatherData.city}
             </h1>
-            <div class="col-sm-4 col-5 current icon-cell pb-3 text-center">
-              <img src={weatherData.imgUrl} alt="" id="current-icon" />
+            <div className="col-sm-4 col-5 current icon-cell pb-3 text-center">
+              <img
+                src={`https://openweathermap.org/img/wn/${weatherData.imgId}@2x.png`}
+                alt={weatherData.desc}
+                id="current-icon"
+              />
             </div>
-            <h2 class="col-sm-4 col-7 current temp-cell text-center">
+            <h2 className="col-sm-4 col-7 current temp-cell text-center">
               <strong>
-                <span id="current-temp-value">{weatherData.temperature}</span>°
+                <span id="current-temp-value">{weatherData.temp}</span>°
                 <small>
-                  <a href="" id="degrees-c" class="unhidden dark-light metric">
+                  <a
+                    href="/"
+                    id="degrees-c"
+                    className="unhidden dark-light metric"
+                  >
                     C{" "}
                   </a>
                   |{" "}
-                  <a href="" id="degrees-f" class="hidden dark-light metric">
+                  <a
+                    href="/"
+                    id="degrees-f"
+                    className="hidden dark-light metric"
+                  >
                     F
                   </a>
                 </small>
               </strong>
             </h2>
           </div>
-          <div class="row">
-            <div class="col-md-12 current">
+          <div className="row">
+            <div className="col-md-12 current">
               <span id="current-day-time">{weatherData.date}</span> ·
-              <span class="description" id="current-description">
+              <span className="description" id="current-description">
                 {" "}
-                {weatherData.description}
+                {weatherData.desc}
               </span>
               <div>
                 Humidity <span id="current-humidity"></span>
