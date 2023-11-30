30d747322b4f4256628442e5f85f35a7e28f9011
em-wb <131539794+em-wb@users.noreply.github.com>
2023-11-30 13:15:10 +0100

    Add search by location

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CurrentWeather from "./CurrentWeather";
 import "./App.css";
@@ -6,8 +6,7 @@ import "./App.css";
 export default function App() {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState("Bordeaux");
-
-  getApiUrl();
+  const [location, setLocation] = useState({ useLocation: false });
 
   function getWeather(response) {
     setWeatherData({
@@ -26,11 +25,19 @@ export default function App() {
 
   function handleSumbit(e) {
     e.preventDefault();
+    setLocation({
+      useLocation: false,
+    });
     getApiUrl();
   }
 
   function getApiUrl() {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+    let url;
+    if (location.useLocation) {
+      url = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.long}&appid=7784a4cd4aa2e0c25ead7bd96d585b8a&units=metric`;
+    } else {
+      url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7784a4cd4aa2e0c25ead7bd96d585b8a&units=metric`;
+    }
     fetch(url).then((response) => {
       if (response.ok) {
         axios.get(url).then(getWeather).then(console.log(weatherData));
@@ -38,46 +45,79 @@ export default function App() {
     });
   }
 
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="app-ctr dark-light">
-          <form
-            onSubmit={handleSumbit}
-            id="search-cities"
-            className="row search-ctr"
-            action=""
-          >
-            <div className="col d-flex">
-              <div className="col-10">
-                <input
-                  onChange={getInput}
-                  id="new-city"
-                  type="text"
-                  placeholder="Enter a city"
-                />
+  function getUserLocation(e) {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition(getLatLong, error);
+  }
+
+  function getLatLong(position) {
+    setLocation({
+      lat: position.coords.latitude,
+      long: position.coords.longitude,
+      useLocation: true,
+    });
+  }
+
+  useEffect(() => {
+    getApiUrl();
+  }, [location]);
+
+  function error(error) {
+    if (error.code) {
+      alert(
+        "☹️ Not found! Please check location services are enabled in your browser to use this feature."
+      );
+    }
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="app-ctr dark-light">
+            <form
+              onSubmit={handleSumbit}
+              id="search-cities"
+              className="row search-ctr"
+              action=""
+            >
+              <div className="col d-flex">
+                <div className="col-10">
+                  <input
+                    onChange={getInput}
+                    id="new-city"
+                    type="text"
+                    placeholder="Enter a city"
+                  />
+                </div>
+                <div className="btn-ctr d-flex col-md-2">
+                  <button type="submit">
+                    <i
+                      className="fa-solid fa-magnifying-glass"
+                      alt="Search"
+                    ></i>
+                  </button>
+                  <button
+                    onClick={getUserLocation}
+                    type="button"
+                    id="current-location-btn"
+                    alt="Current location"
+                  >
+                    <i className="fa-solid fa-location-dot"></i>
+                  </button>
+                </div>
               </div>
-              <div className="btn-ctr d-flex col-md-2">
-                <button type="submit">
-                  <i className="fa-solid fa-magnifying-glass" alt="Search"></i>
-                </button>
-                <button
-                  type="button"
-                  id="current-location-btn"
-                  alt="Current location"
-                >
-                  <i className="fa-solid fa-location-dot"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-          <CurrentWeather weatherData={weatherData} />
+            </form>
+            <CurrentWeather weatherData={weatherData} />
+          </div>
+          <footer>
+            <a href="https://github.com/em-wb/sc-weather-react">Open source</a>{" "}
+            code by Emily
+          </footer>
         </div>
-        <footer>
-          <a href="https://github.com/em-wb/sc-weather-react">Open source</a>{" "}
-          code by Emily
-        </footer>
       </div>
-    </div>
-  );
+    );
+  } else {
+    getApiUrl();
+  }
 }
